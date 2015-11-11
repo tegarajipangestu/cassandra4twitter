0365846f3351e23ad3e73d35c2ede85eb78444df
tegarajipangestu <13512061@std.stei.itb.ac.id>
2015-11-11 19:05:41 +0700

    Init table

--- src/com/tegar/cassandraclient/twitterclient.java
+++ src/com/tegar/cassandraclient/twitterclient.java
@@ -26,7 +26,7 @@ public class twitterclient {
         session = cluster.connect();
     }
     
-    public void createSchema(String keyspace, String replicationStrategy, String replicationFactor) {
+    public void initSchema(String keyspace, String replicationStrategy, String replicationFactor) {
         // Create keyspace
         String query = "CREATE KEYSPACE IF NOT EXISTS " + keyspace +
                 " WITH replication = {'class':'" + replicationStrategy + "', 'replication_factor':"+replicationFactor+"};";
@@ -34,55 +34,11 @@ public class twitterclient {
 
         session.execute("USE "+TWITTER_KEYSPACE + ";");
 
-        // Create table 'users'
-        query = "CREATE TABLE IF NOT EXISTS "+ TABLE_USERS +" (" +
-                "username text PRIMARY KEY, " +
-                "password text" +
-                ")";
-        session.execute(query);
-
-        // Create table 'friends'
-        query = "CREATE TABLE IF NOT EXISTS "+ TABLE_FRIENDS +" (" +
-                " username text, " +
-                " friend text, " +
-                " since timestamp, " +
-                " PRIMARY KEY (username, friend) " +
-                ")";
-        session.execute(query);
-
-        // Create table 'followers'
-        query = "CREATE TABLE IF NOT EXISTS "+ TABLE_FOLLOWERS+" (" +
-                " username text, " +
-                " follower text, " +
-                " since timestamp, " +
-                " PRIMARY KEY (username, follower) " +
-                ")";
-        session.execute(query);
-
-        // Create table 'tweets'
-        query = "CREATE TABLE IF NOT EXISTS "+ TABLE_TWEETS +" (" +
-                " tweet_id uuid PRIMARY KEY, " +
-                " username text, " +
-                " body text " +
-                ")";
-        session.execute(query);
-
-        // Create table 'userline'
-        query = "CREATE TABLE IF NOT EXISTS "+ TABLE_USERLINE +" (" +
-                " username text, " +
-                " time timeuuid, " +
-                " tweet_id uuid, " +
-                " PRIMARY KEY (username, time)" +
-                ") WITH CLUSTERING ORDER BY (time DESC)";
-        session.execute(query);
-
-        // Create table 'timeline'
-        query = "CREATE TABLE IF NOT EXISTS "+ TABLE_TIMELINE  +" (" +
-                " username text, " +
-                " time timeuuid, " +
-                " tweet_id uuid, " +
-                " PRIMARY KEY (username, time)" +
-                ") WITH CLUSTERING ORDER BY (time DESC)";
-        session.execute(query);
+        session.execute("CREATE TABLE IF NOT EXISTS users (username text PRIMARY KEY,password text)");
+        session.execute("CREATE TABLE IF NOT EXISTS friends (username text, friend text, since timestamp, PRIMARY KEY (username, friend))");
+        session.execute("CREATE TABLE IF NOT EXISTS followers (username text,follower text, since timestamp, PRIMARY KEY (username, follower))");
+        session.execute("CREATE TABLE IF NOT EXISTS tweets (tweet_id uuid PRIMARY KEY, username text, body text)");
+        session.execute("CREATE TABLE IF NOT EXISTS userline (username text, time timeuuid, tweet_id uuid, PRIMARY KEY (username, time)) WITH CLUSTERING ORDER BY (time DESC)");
+        session.execute("CREATE TABLE IF NOT EXISTS timeline (username text, time timeuuid, tweet_id uuid, PRIMARY KEY (username, time)) WITH CLUSTERING ORDER BY (time DESC)");
     }
 }
